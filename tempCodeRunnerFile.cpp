cdfe597b26738096dc6bb9c5dd894477b40c95a4
Mekin Jemal <mekinjemal999@gmail.com>
2024-03-21 09:06:00 +0300

    for(datatype name:names) is added

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int arrays[]={3,3,4,5,5,6,5};
+     cout<<arrays[0];
+     //sizeof() =determine the size in bytes of a 
+    //  // variable ,data types ,class ,objects etc
+    //  double gpa=3.5;
+    //  char grade ='F';
+    //  std::cout<<sizeof(gpa)<<"bytes";
